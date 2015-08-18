01a72ae11d609f2a2f07232bead3967db7c2dc6d
yongqiliu <yongqiliu@sohu-inc.com>
2015-08-19 01:45:22 +0800

    11

--- add/static/js/add.js
+++ add/static/js/add.js
@@ -14,7 +14,17 @@ $(document).ready(function(){
 				html += "<option value='"+obj[j]+"'>"+obj[j]+"</option>";
 			}
 			$('#signprovince1').empty();
+            $('#signprovince2').empty();
+            $('#signprovince3').empty();
+            $('#signprovince4').empty();
+            $('#signprovince5').empty();
+            $('#signprovince6').empty();
 			$(html).appendTo('#signprovince1');
+            $(html).appendTo('#signprovince2');
+            $(html).appendTo('#signprovince3');
+            $(html).appendTo('#signprovince4');
+            $(html).appendTo('#signprovince5');
+            $(html).appendTo('#signprovince6');
 
 	});
 
@@ -22,7 +32,7 @@ $(document).ready(function(){
 	var province = $('#signprovince1').val();
         $.get("/add/ajax_school", {province:province}, function(data,status){
 			var html = '';
-			html += '<option >请选择院校</option>';
+			html += '<option value="">请选择院校</option>';
 			for(var j=0;j<data.length;j++){
 				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
 			}
@@ -31,11 +41,76 @@ $(document).ready(function(){
         });
     });
 
+    $('#signprovince2').change(function (){
+	var province = $('#signprovince2').val();
+        $.get("/add/ajax_school", {province:province}, function(data,status){
+			var html = '';
+			html += '<option value="" >请选择院校</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#signcollage2').empty();
+			$(html).appendTo('#signcollage2');
+        });
+    });
+
+    $('#signprovince3').change(function (){
+	var province = $('#signprovince3').val();
+        $.get("/add/ajax_school", {province:province}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择院校</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#signcollage3').empty();
+			$(html).appendTo('#signcollage3');
+        });
+    });
+
+    $('#signprovince4').change(function (){
+	var province = $('#signprovince4').val();
+        $.get("/add/ajax_school", {province:province}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择院校</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#signcollage4').empty();
+			$(html).appendTo('#signcollage4');
+        });
+    });
+
+    $('#signprovince5').change(function (){
+	var province = $('#signprovince5').val();
+        $.get("/add/ajax_school", {province:province}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择院校</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#signcollage5').empty();
+			$(html).appendTo('#signcollage5');
+        });
+    });
+
+    $('#signprovince6').change(function (){
+	var province = $('#signprovince6').val();
+        $.get("/add/ajax_school", {province:province}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择院校</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#signcollage6').empty();
+			$(html).appendTo('#signcollage6');
+        });
+    });
+
     $('#signcollage1').change(function (){
 	var school = $('#signcollage1').val();
         $.get("/add/ajax_major", {school:school}, function(data,status){
 			var html = '';
-			html += '<option >请选择专业</option>';
+			html += '<option  value="">请选择专业</option>';
 			for(var j=0;j<data.length;j++){
 				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
 			}
@@ -53,5 +128,169 @@ $(document).ready(function(){
 			$(html).appendTo('#school1plan6');
         });
     });
+
+    $('#signcollage2').change(function (){
+	var school = $('#signcollage2').val();
+        $.get("/add/ajax_major", {school:school}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择专业</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#school2plan1').empty();
+			$(html).appendTo('#school2plan1');
+            $('#school2plan2').empty();
+			$(html).appendTo('#school2plan2');
+            $('#school2plan3').empty();
+			$(html).appendTo('#school2plan3');
+            $('#school2plan4').empty();
+			$(html).appendTo('#school2plan4');
+            $('#school2plan5').empty();
+			$(html).appendTo('#school2plan5');
+            $('#school2plan6').empty();
+			$(html).appendTo('#school2plan6');
+        });
+    });
+
+    $('#signcollage3').change(function (){
+	var school = $('#signcollage3').val();
+        $.get("/add/ajax_major", {school:school}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择专业</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#school3plan1').empty();
+			$(html).appendTo('#school3plan1');
+            $('#school3plan2').empty();
+			$(html).appendTo('#school3plan2');
+            $('#school3plan3').empty();
+			$(html).appendTo('#school3plan3');
+            $('#school3plan4').empty();
+			$(html).appendTo('#school3plan4');
+            $('#school3plan5').empty();
+			$(html).appendTo('#school3plan5');
+            $('#school3plan6').empty();
+			$(html).appendTo('#school3plan6');
+        });
+    });
+
+    $('#signcollage4').change(function (){
+	var school = $('#signcollage4').val();
+        $.get("/add/ajax_major", {school:school}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择专业</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#school4plan1').empty();
+			$(html).appendTo('#school4plan1');
+            $('#school4plan2').empty();
+			$(html).appendTo('#school4plan2');
+            $('#school4plan3').empty();
+			$(html).appendTo('#school4plan3');
+            $('#school4plan4').empty();
+			$(html).appendTo('#school4plan4');
+            $('#school4plan5').empty();
+			$(html).appendTo('#school4plan5');
+            $('#school4plan6').empty();
+			$(html).appendTo('#school4plan6');
+        });
+    });
+
+    $('#signcollage5').change(function (){
+	var school = $('#signcollage5').val();
+        $.get("/add/ajax_major", {school:school}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择专业</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#school5plan1').empty();
+			$(html).appendTo('#school5plan1');
+            $('#school5plan2').empty();
+			$(html).appendTo('#school5plan2');
+            $('#school5plan3').empty();
+			$(html).appendTo('#school5plan3');
+            $('#school5plan4').empty();
+			$(html).appendTo('#school5plan4');
+            $('#school5plan5').empty();
+			$(html).appendTo('#school5plan5');
+            $('#school5plan6').empty();
+			$(html).appendTo('#school5plan6');
+        });
+    });
+
+    $('#signcollage6').change(function (){
+	var school = $('#signcollage6').val();
+        $.get("/add/ajax_major", {school:school}, function(data,status){
+			var html = '';
+			html += '<option  value="">请选择专业</option>';
+			for(var j=0;j<data.length;j++){
+				html += "<option value='"+data[j]+"'>"+data[j]+"</option>";
+			}
+			$('#school6plan1').empty();
+			$(html).appendTo('#school6plan1');
+            $('#school6plan2').empty();
+			$(html).appendTo('#school6plan2');
+            $('#school6plan3').empty();
+			$(html).appendTo('#school6plan3');
+            $('#school6plan4').empty();
+			$(html).appendTo('#school6plan4');
+            $('#school6plan5').empty();
+			$(html).appendTo('#school6plan5');
+            $('#school6plan6').empty();
+			$(html).appendTo('#school6plan6');
+        });
+    });
+
+
+    $('form:first').bind('submit',function(){
+		//if($.trim(name.val()).length<2 || name.val().length>10){
+			//showPop('姓名输入错误','姓名必须为中文，并且不能少于2或大于10');
+			//focusTarget=name;
+			//return false;
+		//}
+		var name=$('[name=name]'),//姓名
+		student_id=$('[name=student_id]'),//身份证
+		score_big=$('[name=score_big]'),//参加辅导班
+		score_small=$('[name=score_small]'),//邮箱
+		//score_add=$('[name=score_add]'),//手机
+		district=$('[name=district]'),//联系地址
+		school=$('[name=school]')
+
+
+        if($.trim(student_id.val())=='' || ($.trim(student_id.val())!='' && !(/^(([0-9]{1,20}))$/.test(student_id.val())))) {
+                      alert('考生号必须为纯数字');
+		      return false;
+                };
+
+        if(!new RegExp("^[\u4e00-\u9fa5]{2,10}$").test($.trim($('[name=name]').val())))
+        {
+            alert('姓名输入错误','姓名必须为中文，并且不能少于2或大于10');
+			return false;
+        };
+
+        if($.trim(score_big.val())=='' || ($.trim(score_big.val())!='' && (!(/^(([0-9]{1,3}))$/.test(score_big.val())))) || parseInt(score_big.val()) > 700){
+                      alert('总分不符合要求');
+		      return false;
+                };
+
+        if($.trim(score_small.val())=='' || ($.trim(score_small.val())!='' && (!(/^(([0-9]{1,3}))$/.test(score_small.val())))) || parseInt(score_big.val()) > 700){
+                      alert('实考分不符合要求');
+		      return false;
+                };
+
+		if($.trim(district.val()) == ""){
+			alert('考生所在地为必填项');
+			return false;
+		}
+
+        if($.trim(school.val()) == ""){
+			alert('考生学校为必填项');
+			return false;
+		}
+	});
+
 
 });
--- add/views.py
+++ add/views.py
@@ -1,5 +1,6 @@
+# coding: UTF-8
 from django.shortcuts import get_object_or_404, render
-from models import StudentBaseInfo, SchoolPlan
+from models import StudentBaseInfo, SchoolPlan, StudentPlanSchool, StudentPlanSchoolDepartment
 from django.http import JsonResponse
 import json
 # Create your views here.
@@ -54,12 +55,15 @@ def model(request):
     if model_type == "1": #add user
         studentid = request.POST['student_id']
 
+        print "studentid", studentid
+        if studentid == "":
+            return render(request, 'add/user.html', )
 
         p = StudentBaseInfo.objects.filter(student_id=studentid)
 
         if p:
             return render(request, 'add/userdetail.html', {
-                'error_message': "user has already exist!",
+                'error_message': "user id has already exist!",
                 })
         else:
             name = request.POST['name']
@@ -70,12 +74,121 @@ def model(request):
             district = request.POST['district']
             school = request.POST['school']
 
+            if name == "" or score_big == "" or score_small == "" or score_add == "" or district == "" or school == "":
+                return render(request, 'add/userdetail.html', {
+                'error_message': "所有字段不能为空",
+                })
             baseinfo = StudentBaseInfo(student_id=studentid, name=name, sex="1", score_big=score_big, score_small=score_small,score_add=score_add,district=district,school=school)
             baseinfo.save()
+
+
+            signcollage1 = ""
+            signcollage2 = ""
+            signcollage3 = ""
+            signcollage4 = ""
+            signcollage5 = ""
+            signcollage6 = ""
+
+            signprovince1 = request.POST['signprovince1']
+            if signprovince1 != '0':
+                signcollage1 = request.POST['signcollage1']
+
+            signprovince2 = request.POST['signprovince2']
+            if signprovince2 != '0':
+                signcollage2 = request.POST['signcollage2']
+
+            signprovince3 = request.POST['signprovince3']
+            if signprovince3 != '0':
+                signcollage3 = request.POST['signcollage3']
+
+            signprovince4 = request.POST['signprovince4']
+            if signprovince4 != '0':
+                signcollage4 = request.POST['signcollage4']
+
+            signprovince5 = request.POST['signprovince5']
+            if signprovince5 != '0':
+                signcollage5 = request.POST['signcollage5']
+
+            signprovince6 = request.POST['signprovince6']
+            if signprovince6 != '0':
+                signcollage6 = request.POST['signcollage6']
+
+            planSchool = StudentPlanSchool(student_id=studentid, school1=signcollage1, school2=signcollage2,
+                                           school3=signcollage3, school4=signcollage4, school5=signcollage5,
+                                           school6=signcollage6)
+            planSchool.save()
+
+
+            if signcollage1 != "":
+                school1plan1 = request.POST['school1plan1']
+                school1plan2 = request.POST['school1plan2']
+                school1plan3 = request.POST['school1plan3']
+                school1plan4 = request.POST['school1plan4']
+                school1plan5 = request.POST['school1plan5']
+                school1plan6 = request.POST['school1plan6']
+                canadjust1 = request.POST['canadjust1']
+                planDepart = StudentPlanSchoolDepartment(student_id=studentid, school=signcollage1, depa1=school1plan1, depa2=school1plan2, depa3=school1plan3,
+                                                         depa4=school1plan4, depa5=school1plan5, depa6=school1plan6, canadjust=canadjust1)
+                planDepart.save()
+            if signcollage2 != "":
+                school2plan1 = request.POST['school2plan1']
+                school2plan2 = request.POST['school2plan2']
+                school2plan3 = request.POST['school2plan3']
+                school2plan4 = request.POST['school2plan4']
+                school2plan5 = request.POST['school2plan5']
+                school2plan6 = request.POST['school2plan6']
+                canadjust2 = request.POST['canadjust2']
+                planDepart = StudentPlanSchoolDepartment(student_id=studentid, school=signcollage2, depa1=school2plan1, depa2=school2plan2, depa3=school2plan3,
+                                                         depa4=school2plan4, depa5=school2plan5, depa6=school2plan6, canadjust=canadjust1)
+                planDepart.save()
+            if signcollage3 != "":
+                school3plan1 = request.POST['school3plan1']
+                school3plan2 = request.POST['school3plan2']
+                school3plan3 = request.POST['school3plan3']
+                school3plan4 = request.POST['school3plan4']
+                school3plan5 = request.POST['school3plan5']
+                school3plan6 = request.POST['school3plan6']
+                canadjust3 = request.POST['canadjust3']
+                planDepart = StudentPlanSchoolDepartment(student_id=studentid, school=signcollage3, depa1=school3plan1, depa2=school3plan2, depa3=school3plan3,
+                                                         depa4=school3plan4, depa5=school3plan5, depa6=school3plan6, canadjust=canadjust1)
+                planDepart.save()
+            if signcollage4 != "":
+                school4plan1 = request.POST['school4plan1']
+                school4plan2 = request.POST['school4plan2']
+                school4plan3 = request.POST['school4plan3']
+                school4plan4 = request.POST['school4plan4']
+                school4plan5 = request.POST['school4plan5']
+                school4plan6 = request.POST['school4plan6']
+                canadjust4 = request.POST['canadjust4']
+                planDepart = StudentPlanSchoolDepartment(student_id=studentid, school=signcollage4, depa1=school4plan1, depa2=school4plan2, depa3=school4plan3,
+                                                         depa4=school4plan4, depa5=school4plan5, depa6=school4plan6, canadjust=canadjust1)
+                planDepart.save()
+            if signcollage5 != "":
+                school2plan1 = request.POST['school2plan1']
+                school2plan2 = request.POST['school2plan2']
+                school2plan3 = request.POST['school2plan3']
+                school2plan4 = request.POST['school2plan4']
+                school2plan5 = request.POST['school2plan5']
+                school2plan6 = request.POST['school2plan6']
+                canadjust5 = request.POST['canadjust5']
+                planDepart = StudentPlanSchoolDepartment(student_id=studentid, school=signcollage5, depa1=school2plan1, depa2=school2plan2, depa3=school2plan3,
+                                                         depa4=school2plan4, depa5=school2plan5, depa6=school2plan6, canadjust=canadjust1)
+                planDepart.save()
+            if signcollage6 != "":
+                school6plan1 = request.POST['school6plan1']
+                school6plan2 = request.POST['school6plan2']
+                school6plan3 = request.POST['school6plan3']
+                school6plan4 = request.POST['school6plan4']
+                school6plan5 = request.POST['school6plan5']
+                school6plan6 = request.POST['school6plan6']
+                canadjust6 = request.POST['canadjust6']
+                planDepart = StudentPlanSchoolDepartment(student_id=studentid, school=signcollage6, depa1=school6plan1, depa2=school6plan2, depa3=school6plan3,
+                                                         depa4=school6plan4, depa5=school6plan5, depa6=school6plan6, canadjust=canadjust1)
+                planDepart.save()
+
             return render(request, 'add/userdetail.html', {
                 'baseinfo': baseinfo,
                 })
-
 
 
 
